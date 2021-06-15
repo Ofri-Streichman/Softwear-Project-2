174443c327f91722cf4df106e330bf8fb0b2ef30
ValeriaVaisman <84072848+ValeriaVaisman@users.noreply.github.com>
2021-06-15 17:22:00 +0300

    Update kmeans.c

--- kmeans.c
+++ kmeans.c
@@ -3,15 +3,12 @@
                              <stdio.h>, <string.h>, <errno.h>, <limits.h>, <assert.h> and <stdlib.h> (if available). */
 #include <math.h>         /* includeֲ <Python.h> has to be before any standard headers are included */
 
-
-
 typedef struct {
     double * vector_sum;
     int size;
 
 }cluster;
 
-double* array_copy(double*,const double*, int);
 void array_2dim_copy(double **, double **,int ,int );
 int array_equal(double** , double** , int ,int ) ;
 double diff(const double *,const double* ,int);
@@ -37,16 +34,8 @@ cluster* cluster_init(cluster * clusters, int k, int d){
     return clusters;
 }
 
-/*copy array to other array*/
-double* array_copy(double* array1,const double* array2,  int d){
-    int k;
-    for(k=0;k<d;k++){
-        array1[k]=array2[k];
-    }
-    return array1;
-}
 
-/*copys 2d array to other 2d array*/
+/*copies 2d array to other 2d array*/
 void array_2dim_copy(double **Double, double **Double1,int K,int d) {
     int l,m;
     for(l=0;l<K;l++){
@@ -130,6 +119,7 @@ void reset_cluster(cluster* cluster1, int k,  int d) {
     }
 }
 
+/* the main function that calculates the centroids */
 double** main_c(int K,int N,int d,int max_iter, double ** vectors,double ** centroids) {
 
     int cnt;
@@ -137,9 +127,9 @@ double** main_c(int K,int N,int d,int max_iter, double ** vectors,double ** cent
     int r;
     int p;
     int x, b;
-    double *distance;
+    double* distance;
     cluster *clusters;
-    double **prev_centroids;
+    double** prev_centroids;
 
     distance = (double *) malloc(K * sizeof(double));
     assert(distance != NULL);
@@ -175,15 +165,10 @@ double** main_c(int K,int N,int d,int max_iter, double ** vectors,double ** cent
         cnt++;
     }
 
+/* free the memory */
+
     free(distance);
     distance = NULL;
-
-    for (p = 0; p < N; p++) {
-        free(vectors[p]);
-        vectors[p] = NULL;
-    }
-    free(vectors);
-    vectors = NULL;
 
     for (p = 0; p < K; p++) {
         free(prev_centroids[p]);
@@ -201,116 +186,137 @@ double** main_c(int K,int N,int d,int max_iter, double ** vectors,double ** cent
 
     return centroids;
 }
-    /*
+
+/*
  * API functions
  */
 
+static PyObject* fit_capi(PyObject *self, PyObject *args)
+{
+    PyObject *PyList1, *PyList2;
+    PyObject *item, *item2;
+    int k,n,d,max;
+    double ** vectors;
+    double ** centroids;
+    double ** new_centroids;
+    int i,j;
+    int p;
 
-    static PyObject* fit_capi(PyObject *self, PyObject *args)
-    {
-        PyObject *_list, *_list2;
-        PyObject *item, *item2, *item3;
-        int k,n,d,max;
-        double ** vectors;
-        double ** centroids;
-        double ** new_centroids;
-        double ** more_centroids;
-        int i,j;
+    if(!PyArg_ParseTuple(args, "iiiiOO", &k,&n,&d,&max,&PyList1,&PyList2)) {
+        return NULL;
+    }
 
-        if(!PyArg_ParseTuple(args, "iiiiOO", &k,&n,&d,&max,&_list,&_list2)) {
+    PyObject *result_lst = PyList_New(k);
+    vectors = (double **) malloc(n* sizeof(double *));
+    assert(vectors != NULL);
+    for (p = 0; p < n; p++) {
+        vectors[p] = (double *) malloc(d * sizeof(double));
+        assert(vectors[p] != NULL);
+    }
+    centroids = (double **) malloc(k * sizeof(double *));
+    assert(centroids != NULL);
+    for (p = 0; p < k; p++) {
+        centroids[p] = (double *) malloc(d * sizeof(double));
+        assert(centroids[p] != NULL);
+    }
+
+
+    /* checking if the PyObjects are lists */
+    if (!PyList_Check(PyList1)||!PyList_Check(PyList2)||!PyList_Check(result_lst)) {
+        return NULL;
+    }
+
+    /* making vectors array and centroids array with the PyObjects */
+    for (i = 0; i < k; i++) {
+        item = PyList_GetItem(PyList2, i);
+        PyObject *item_inside;
+        for (j = 0; j < d; j++) {
+            item_inside = PyList_GetItem(item, j);
+            centroids[i][j] = PyFloat_AsDouble(item_inside);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        item2 = PyList_GetItem(PyList1, i);
+        PyObject *item_inside;
+        for (j = 0; j < d; j++) {
+            item_inside = PyList_GetItem(item2, j);
+            vectors[i][j] = PyFloat_AsDouble(item_inside);
+        }
+    }
+
+    /* calculating the final centroids */
+    new_centroids = main_c(k,n,d,max, vectors, centroids);
+
+    /* making PyList of the final centroids */
+    for (i = 0; i < k; i++) {
+        PyObject *vec_lst = PyList_New(d);
+        if (!vec_lst) {
             return NULL;
         }
-
-        PyObject *result_lst = PyList_New(k);
-        vectors = (double **) malloc(n* sizeof(double *));
-        centroids = (double **) malloc(k * sizeof(double *));
-        new_centroids = (double **) malloc(k * sizeof(double *));
-        more_centroids = (double **) malloc(k * sizeof(double *));
-
-        /* Is it a list? */
-        if (!PyList_Check(_list)||!PyList_Check(_list2)||!PyList_Check(result_lst)) {
-            return NULL;
-        }
-
-        for (i = 0; i < k; i++) {
-            item = PyList_GetItem(_list2, i);
-            PyObject *item_inside;
-            double* centroid = malloc(d * sizeof(double));
-            assert(centroid != NULL);
-            for (j = 0; j < d; j++) {
-                item_inside = PyList_GetItem(item, j);
-                centroid[j] = PyFloat_AsDouble(item_inside);
-            }
-            centroids[i] = centroid;
-        }
-
-        for (i = 0; i < n; i++) {
-            item2 = PyList_GetItem(_list, i);
-            PyObject *item_inside;
-            double* x_i = malloc(d * sizeof(double));
-            assert(x_i != NULL);
-            for (j = 0; j < d; j++) {
-                item_inside = PyList_GetItem(item2, j);
-                x_i[j] = PyFloat_AsDouble(item_inside);
-            }
-            vectors[i] = x_i;
-        }
-
-        new_centroids = main_c(k,n,d,max, vectors, centroids);
-
-        for (i = 0; i < k; i++) {
-            PyObject *vec_lst = PyList_New(d);
-            if (!vec_lst) {
+        for (j = 0; j < d; j++) {
+            PyObject *num = PyFloat_FromDouble(new_centroids[i][j]);
+            if (!num) {
+                Py_DECREF(num);
                 return NULL;
             }
-            for (j = 0; j < d; j++) {
-                PyObject *num = PyFloat_FromDouble(new_centroids[i][j]);
-                if (!num) {
-                    Py_DECREF(num);
-                    return NULL;
-                }
-                PyList_SetItem(vec_lst,j,num);
-            }
-            PyList_SetItem(result_lst,i,vec_lst);
+            PyList_SetItem(vec_lst,j,num);
         }
-        return result_lst;
+        PyList_SetItem(result_lst,i,vec_lst);
     }
+
+    /* free the memory */
+    for (p = 0; p < n; p++) {
+        free(vectors[p]);
+        vectors[p] = NULL;
+    }
+    free(vectors);
+    vectors = NULL;
+
+    for (p = 0; p < k; p++) {
+        free(centroids[p]);
+        centroids[p] = NULL;
+    }
+    free(centroids);
+    centroids = NULL;
+
+    return result_lst;
+}
 
 /*
  * This array tells Python what methods this module has.
  * We will use it in the next structure
  */
 
-    static PyMethodDef capiMethods[] = {
-            {"fit",                   /* the Python method name that will be used */
-                    (PyCFunction)  fit_capi, /* the C-function that implements the Python function and returns static PyObject*  */
-                         METH_VARARGS,           /* flags indicating parameters
+static PyMethodDef capiMethods[] = {
+        {"fit",                   /* the Python method name that will be used */
+                (PyCFunction)  fit_capi, /* the C-function that implements the Python function and returns static PyObject*  */
+                     METH_VARARGS,           /* flags indicating parameters
 accepted for this function */
-                            PyDoc_STR("A geometric series up to n. sum_up_to_n(z^n)")}, /*  The docstring for the function */
-            {NULL, NULL, 0, NULL}     /* The last entry must be all NULL as shown to act as a
+                        PyDoc_STR("A geometric series up to n. sum_up_to_n(z^n)")}, /*  The docstring for the function */
+        {NULL, NULL, 0, NULL}     /* The last entry must be all NULL as shown to act as a
                                  sentinel. Python looks for this entry to know that all                                 of the functions for the module have been defined. */
-    };
+};
 
 
 
 
 /* This initiates the module using the above definitions. */
-    static struct PyModuleDef moduledef = {
-            PyModuleDef_HEAD_INIT,
-            "mykmeanssp", /* name of module */
-            NULL, /* module documentation, may be NULL */
-            -1,  /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
-            capiMethods /* the PyMethodDef array from before containing the methods of the extension */
-    };
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "mykmeanssp", /* name of module */
+        NULL, /* module documentation, may be NULL */
+        -1,  /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+        capiMethods /* the PyMethodDef array from before containing the methods of the extension */
+};
 
-    PyMODINIT_FUNC
-    PyInit_mykmeanssp(void)
-    {
-        PyObject *n;
-        n = PyModule_Create(&moduledef);
-        if (!n) {
-            return NULL;
-        }
-        return n;
+PyMODINIT_FUNC
+PyInit_mykmeanssp(void)
+{
+    PyObject *n;
+    n = PyModule_Create(&moduledef);
+    if (!n) {
+        return NULL;
     }
-
+    return n;
+}
